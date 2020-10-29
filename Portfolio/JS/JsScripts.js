1fff539ce6c361d68dfc73e55c6181eee8a726ea
Isaac Vorköper <isaaclennart@gmail.com>
2020-10-29 14:48:46 +0100

    Navigation mellem siderne lavet.

--- Portfolio/JS/JsScripts.js
+++ Portfolio/JS/JsScripts.js
@@ -9,13 +9,25 @@ $(function() {
     }, 50);
 
 
-
-
 });
 
 function one() {
     document.getElementById('1').style.opacity = '0';
     setInterval(function(){ location.href = "AboutMe.html" }, 1000);
 }
+
+/*Skift af side*/
+$(".panel").on("click", function (){
+    location.href = this.id
+    $(this.id + "page").css('font-size', '1.7vw');
+    $(this.id + "page").css('text-decoration', 'underline');
+})
+
+$('#pagechanger p').click(function(event) {
+    event.preventDefault();
+    $('#pagechanger p').removeClass('active');
+    $(this).toggleClass('active');
+});
+
 
 
